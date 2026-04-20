6434f8fa85f57e0415b546a3e8c7716707205ff2
Riddhi <agarwal.riddhi72@gmail.com>
2026-04-19 22:08:25 -0400

    have aliens also drop when hitting edges

--- alien.py
+++ alien.py
@@ -25,7 +25,7 @@ class Alien(Sprite):
         temp_speed = self.settings.fleet_speed
         if self.check_edges():
             self.settings.fleet_direction *= -1
-
+            self.x -= self.settings.fleet_drop_speed
         self.y += temp_speed * self.settings.fleet_direction
         self.rect.y = self.y
         self.rect.x = self.x
