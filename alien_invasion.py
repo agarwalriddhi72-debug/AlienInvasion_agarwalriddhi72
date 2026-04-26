21832d46b3e1351f7c28c902625bffc2fe55a59c
Riddhi <agarwal.riddhi72@gmail.com>
2026-04-26 00:30:27 -0400

    p3 - split settings into dynamic and static and added increase difficulty

--- alien_invasion.py
+++ alien_invasion.py
@@ -30,6 +30,7 @@ class AlienInvasion:
         """Initializes the game, including settings, screen, background, and ship."""
         pygame.init()
         self.settings = Settings()
+        self.settings.initialize_dynamic_settings()
         self.game_stats = GameStats(self.settings.starting_ship_count)
 
         self.screen = pygame.display.set_mode((self.settings.screen_w, self.settings.screen_h))
